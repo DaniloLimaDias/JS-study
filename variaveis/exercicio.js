f3e867adc12ab26a1dc0fc5bd60236377934f434
danilo Lima Dias <danilo743377@gmail.com>
2020-10-27 14:08:05 -0300

    exercicios

---
+++ variaveis/exercicio.js
@@ -0,0 +1,10 @@
+// Declarar uma variável com seu nome
+var meuNome = 'Danilo';
+//Declarar uma variável com a sua idade
+var minhaIdade = 22;
+//Declarar uma variável com a sua comida favorita e não atribuir valor
+var comidaFavorita;
+//atribuir valor a sua comida favorita
+comidaFavorita = "bife"
+//declarar 5 variáveis diferentes sem valores
+var soma ,media ,curso, conta, soma;
