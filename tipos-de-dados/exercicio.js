57daf7ea6aee5b301b75995e791afa418241dc46
danilo Lima Dias <danilo743377@gmail.com>
2020-10-27 15:18:55 -0300

    tipos de dados js com exercicios

---
+++ tipos-de-dados/exercicio.js
@@ -0,0 +1,17 @@
+// declarar uma variavel contendo uma string
+var cidade = 'Rio de janeiro'
+// declarar uma variavel conteudo um número dentro de uma string
+var numero = "21"
+// declare uma variavel com a sua idade
+var idade  = 22
+//declare duas variaveis, uma com sua nome
+//e outro com seu sobrenome e some as mesmas
+var primeiroNome = 'danilo'
+var sobreNome = 'lima'
+var nomeCompleto = `Meu nome é ${primeiroNome} ${sobreNome} `
+console.log(nomeCompleto)
+
+//coloque a seguinte frase em uma variavel: It's time
+var frese = "It's time"
+// verifique o tipo da variavel que contem o seu nome
+console.log(typeof nomeCompleto)
---
+++ tipos-de-dados/index.js
@@ -0,0 +1,28 @@
+var idade = 28;
+var simbolo = Symbol();
+console.log(typeof simbolo);
+
+var nome = 'Danilo';
+var sobrenome = 'lima'
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nomeCompleto);
+
+var gols = 1000;
+var frase = 'romario fez' + gols + 'gols';
+console.log(typeof frase);
+
+var ano = '2018';
+var mes = 8;
+console.log(ano + mes);
+
+var melhor = 'teste';
+var frase1 = 'esse é o \"melhor\" jogo';
+console.log(frase1)
+
+var gols = 1000;
+var frase2 = 'romario fez' + gols + 'gols'
+var frase3 = `romario fez ${gols} gols`
+console.log(frase3)
+
+
