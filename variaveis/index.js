6e5dc135a50d57a57596df121d0c29927f1cbfd3
Danilo Lima Dias <danilo743377@gmail.com>
2020-10-27 14:03:23 -0300

    estudando javascript

---
+++ variaveis/index.js
@@ -0,0 +1,53 @@
+var nome = 'danilo';
+var idade = 22;
+var possuoFaculdade = true;
+
+console.log(nome, idade, possuoFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'Danilo',
+  cidade = 'rio de janeiro';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+var comida;
+console.log(comida);
+comida = 'pizza';
+console.log(comida);
+
+let time = 'vasco';
+time = 'flamengo';
+
+console.log(time);
+
+var time2 = 'vasco';
+var time2 = 'flamengo';
+
+console.log(time);
+
+const time3 = 'vasco';
+// const time2 = 'flamengo';
+
+console.log(time);
+
+
+// --------exercicios--------
+
+// Declarar uma variável com seu nome
+var meuNome = 'Danilo';
+//Declarar uma variável com a sua idade
+var minhaIdade = 22;
+//Declarar uma variável com a sua comida favorita e não atribuir valor
+var comidaFavorita;
+//atribuir valor a sua comida favorita
+comidaFavorita = "bife"
+//declarar 5 variáveis diferentes sem valores
+var soma ,media ,curso, conta, soma;
