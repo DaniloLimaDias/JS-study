06c3fd8e855a7168e53d3865ad7775a42db9c9db
danilo Lima Dias <danilo743377@gmail.com>
2020-11-05 15:50:59 -0300

    metodos de numbers

---
+++ metodos-de-numbers/index.js
@@ -0,0 +1,8 @@
+let n = "10.5488";
+
+let res = n.toString(); //transforma um número em string
+// res = n.toFixed(2);
+res = parseInt(n) + 5 // string de numero em número sem as casas decimais
+res = parseFloat(n) + 5 // string de numero em numero com as casas decimais 
+
+console.log(res)
