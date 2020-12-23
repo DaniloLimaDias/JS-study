3ab7bb2772d99370122033d91b0f5d9def992695
danilo Lima Dias <danilo743377@gmail.com>
2020-12-23 20:48:35 -0300

    arrays

---
+++ arrays/index.js
@@ -0,0 +1,10 @@
+const nomes = ['danilo', 'Maria', 'joana', 'rasana'];
+const novo = nomes.slice(1, -2);
+console.log(novo);
+
+const nome = 'Luiz Otaviao mirando'
+const nomes = nome.split(' ');
+
+const nomes = [ 'Danilo', 'Lima', 'Dias' ];
+const nome = nomes.join(' ');
+console.log(nome);
