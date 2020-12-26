1cdbeee93c1a06bdaee3a5dbbd1d6df5d237505f
danilo Lima Dias <danilo743377@gmail.com>
2020-12-25 21:32:17 -0300

    método splice

---
+++ splice/index.js
@@ -0,0 +1,14 @@
+const nomes = ['Maria', 'João', 'Eduardo', 'Danilo'];
+
+//Unshift
+nomes.splice(nomes.length, 0, 'lima', 'dias');
+console.log(nomes);
+
+//como o splice funciona
+//nome.splice(indece atual, delete, alemento1, alemento2)
+
+//pop
+// const removidos = nomes.splice(-1, 1);
+
+//shift
+// const removidos = nomes.splice(0, 1)
